145be5b8d7b14b14421c190f8f742601c62a01b0
Jaweria Shoukat <jaweeriashoukat@gmail.com>
2022-08-18 13:58:40 +0500

    index page pdated

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,49 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import { BsChevronDown } from 'react-icons/bs'
+import OurCommunity from './our-community'
 
 export default function Home() {
   return (
     <>
       {/* Banner Section Start*/}
       <section>
-        <div className='container mx-auto min-h-screen h-full py-20 grid md:grid-cols-2 grid-cols-1 items-center gap-4'>
-          <div className="p-4 flex flex-col justify-between">
-            <div>
-              <h1 className='text-6xl font-thin font-interRegular'>The <span className='text-skin-primary font-bold'>Future</span> of Personal Investment.</h1>
-              <p className='text-xl font-medium mt-3 font-interRegular'>From building your diversified long-term portfolio and tracking your assets to finding investment ideas, we <br></br>got you covered.</p>
+        <div className='container mx-auto lg:h-[800px] pb-10 pt-0 grid lg:grid-cols-3 grid-cols-1 items-center gap-4 mt-20'>
+          <div className="flex flex-col justify-between h-full px-4 pt-10 pb-0 lg:pt-20 lg:pb-24">
+            <div className='z-10'>
+              <h1 className='md:text-6xl text-[32px] leading-9 font-medium font-interRegular'>The <span className='text-skin-primary font-bold'>Future</span> of <span className='md:whitespace-nowrap'>Personal Investment.</span></h1>
+              <p className='md:text-xl text-[15px] font-medium mt-3 font-interRegular'>From building your diversified long-term portfolio and tracking your assets to finding investment ideas, we got you covered.</p>
             </div>
             <Link href="#">
-              <a className='flex space-x-2 items-center text-3xl font-semibold mt-5 font-interMedium'><span className='text-skin-primary animate-bounce'><BsChevronDown strokeWidth={2} /> </span> <span>Join Waitlist</span></a>
+              <a className='md:flex hidden space-x-2 items-center text-3xl font-semibold mt-5 font-interMedium'><span className='text-skin-primary animate-bounce'><BsChevronDown strokeWidth={2} /> </span> <span>Join Waitlist</span></a>
             </Link>
           </div>
-          <div className="p-4">
-            <Image src="/images/mainImg.png" alt='mainImg.png' width={1075} height={560}></Image>
+          <div className="float-right col-span-2 md:p-4 overflow-x-hidden">
+            <div className='md:block hidden'>
+              <video
+                width="1000"
+                height="800"
+                autoPlay="autoplay"
+                className=""
+                loop
+                muted
+              >
+                <source src="/Videos/video.mp4" type="video/mp4" />
+              </video>
+            </div>
+            <figure className="hero-mobile-banner md:hidden relative h-[550px]">
+              <Image
+                src="/images/hero-mobile.png"
+                alt="Mobile Banner"
+                layout='fill'
+                className="w-full"
+              />
+            </figure>
+            <div className='md:hidden block mx-4'>
+              <Link href="#">
+                <a className='flex space-x-2 items-center text-[20px] lg:mt-0 font-semibold mt-20 font-interMedium'><span className='text-skin-primary animate-bounce'><BsChevronDown strokeWidth={2} /> </span> <span>Join Waitlist</span></a>
+              </Link>
+            </div>
           </div>
         </div>
       </section>
@@ -27,24 +52,30 @@ export default function Home() {
 
       {/* Jion Us Section Start*/}
       <section className='bg-skin-primary'>
-        <div className='container min-h-screen h-full mx-auto py-20 grid md:grid-cols-2 grid-cols-1 items-center gap-8'>
-          <div className="p-4">
-            <h1 className='text-6xl font-normal text-[#D1C6FF] font-interRegular'><span className='text-skin-light font-bold'>Join </span> Waitlist</h1>
-            <p className='text-xl text-skin-light font-normal mt-3 font-interRegular'>From building your diversified long-term portfolio and <br></br>tracking you.</p>
+        <div className='container lg:h-[800px] mx-auto lg:py-20 py-12 grid md:grid-cols-2 grid-cols-1 items-center gap-8'>
+          <div className="p-4 md:order-first order-last lg:mt-0 mt-10">
+            <h1 className='md:text-6xl text-[32px] leading-9 font-normal text-[#D1C6FF] font-interRegular'><span className='text-skin-light font-bold'>Join </span> Waitlist</h1>
+            <p className='md:text-xl text-[15px] text-skin-light font-normal mt-3 font-interRegular max-w-[500px]'>From building your diversified long-term portfolio and tracking you.</p>
             <div className='mt-8'>
-              <form className='md:flex md:space-x-2 md:space-y-0 space-x-0 space-y-2'>
-                <input type="email" id="email" placeholder='Enter Email' name="email" className='font-interRegular bg-skin-light text-skin-muted text-2xl font-normal outline-[#6F49DD] focus:outline-[#6F49DD] py-3 px-4 rounded-md md:w-[395px] w-full' />
-                <button type="submit" className='font-interMedium bg-skin-dark text-skin-light hover:opacity-75 text-2xl font-semibold flex items-center py-3 px-12 rounded-md'>Join</button>
+              <form className='md:flex md:space-x-2 md:space-y-0 space-x-0 space-y-4'>
+                <input type="email" id="email" placeholder='Enter Email' name="email" className='font-interRegular bg-skin-light text-skin-muted md:text-2xl text-base font-normal outline-[#6F49DD] focus:outline-[#6F49DD] py-3 px-4 rounded-md md:w-3/4 w-full' />
+                <button type="submit" className='md:hidden text-[#D1C6FF] font-interRegular border border-[#D1C6FF] bg-transparent hover:bg-skin-buttonAccent hover:border-transparent md:text-base text-sm font-normal flex items-center py-3 px-12 rounded-md captcha'>I am not a robot</button>
+                <button type="submit" className='font-interMedium bg-skin-dark text-skin-light hover:opacity-75 md:text-2xl text-lg font-semibold py-3 md:w-1/4 w-full rounded-md'>Join</button>
               </form>
-              <button type="submit" className='text-[#D1C6FF] font-interRegular border border-[#D1C6FF] bg-transparent hover:bg-skin-buttonAccent hover:border-transparent text-base font-normal flex items-center py-3 px-12 rounded-md mt-5'>I am not a robot</button>
+              <button type="submit" className='hidden text-[#D1C6FF] font-interRegular border border-[#D1C6FF] bg-transparent hover:bg-skin-buttonAccent hover:border-transparent md:text-base text-sm font-normal md:flex items-center py-3 px-12 rounded-md mt-5'>I am not a robot</button>
             </div>
           </div>
           <div className="p-4">
-            <iframe class="w-full aspect-video ..." src="http://localhost:3000/images/video.mp4"></iframe>
+            <h1 className='md:hidden block text-[32px] leading-9 md:text-6xl font-normal text-[#D1C6FF] font-interRegular mb-8'>What is <br></br><span className='text-skin-light font-bold'>Fundgazer?</span></h1>
+            <iframe class="w-full aspect-video rounded-lg" src="https://www.youtube.com/embed/tgbNymZ7vqY"></iframe>
           </div>
         </div>
       </section>
       {/* Jion Us Section End*/}
+
+      <div className='lg:hidden block'>
+        <OurCommunity />
+      </div>
 
     </>
   )
--- pages/our-community.js
+++ pages/our-community.js
@@ -1,49 +1,63 @@
-import {BsDiscord} from 'react-icons/bs'
-import {FaTelegramPlane} from 'react-icons/fa'
-import {BiPolygon} from 'react-icons/bi'
-import {FiLayers} from 'react-icons/fi'
-import {FiClock} from 'react-icons/fi'
+import { BsDiscord } from 'react-icons/bs'
+import { FaTelegramPlane } from 'react-icons/fa'
+import { BiPolygon } from 'react-icons/bi'
+import { FiLayers } from 'react-icons/fi'
+import { FiClock } from 'react-icons/fi'
+import { FaAngleRight } from 'react-icons/fa'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 export default function OurCommunity() {
-  return (
-    <>
-    {/* Banner Section Start*/}
-    <section>
-        <div className='container mx-auto pt-40 pb-20 grid md:grid-cols-2 grid-cols-1 gap-8 items-center'>
-            <div className="p-4">
-                <h1 className='md:text-6xl text-4xl font-normal font-interRegular'><span className='text-skin-primary font-bold font-interBold'>Join</span> Our Community</h1>
-                <p className='md:text-xl text-base font-medium mt-3 font-interRegular'>We are all about giving personal investors the tools and resources they need, to make informed decisions.</p>
-            </div>
-            <div className="flex items-center md:justify-end justify-center gap-5 p-4">
-                <button className='font-interRegular bg-skin-dark text-skin-light hover:opacity-75 md:text-2xl text-lg font-semibold flex items-center space-x-3 py-3 px-7 rounded-md'><BsDiscord /> <span>Discord</span> </button>
-                <button className='font-interRegular bg-skin-buttonAccent hover:bg-skin-buttonMuted text-skin-light md:text-2xl text-lg font-semibold flex items-center space-x-3 py-3 px-7 rounded-md'><FaTelegramPlane /> <span>Telegram</span> </button>
-            </div>
-        </div>
-    </section>
-    {/* Banner Section End*/}
 
-{/* Resources Section Start*/}
-    <section className='bg-[#E9E1FF]'>
-        <div className='container mx-auto py-12 grid md:grid-cols-4 grid-cols-1 gap-2'>
-            <div className='p-4'>
-                <h5 className='font-interBold md:text-2xl text-xl font-bold flex space-x-2 items-center mb-3'><span className='text-skin-primary'><BiPolygon strokeWidth={2}/> </span> <span>Belong</span></h5>
-                <p className='font-interRegular md:text-xl text-base font-normal text-skin-muted'>Get real-time advice and support from a community of experienced investors and don’t forget to give back!</p>
-            </div>
-            <div className='p-4'>
-                <h5 className='font-interBold md:text-2xl text-xl font-bold flex space-x-2 items-center mb-3'><span className='text-skin-primary'><FiLayers strokeWidth={3}/> </span> <span>Resources</span></h5>
-                <p className='font-interRegular md:text-xl text-base font-normal text-skin-muted'>Learn about finance, stocks, crypto and psychology behind building investment strategies and systems.</p>
-            </div>
-            <div className='p-4'>
-                <h5 className='font-interBold md:text-2xl text-xl font-bold flex space-x-2 items-center mb-3'><span className='text-skin-primary'><BiPolygon strokeWidth={2}/> </span> <span>Black Box</span></h5>
-                <p className='font-interRegular md:text-xl text-base font-normal text-skin-muted'>Stay ahead of the curve with market alerts powered by our proprietary algorithms.</p>
-            </div>
-            <div className='p-4'>
-                <h5 className='font-interBold md:text-2xl text-xl font-bold flex space-x-2 items-center mb-3'><span className='text-skin-primary'><FiClock strokeWidth={3}/> </span> <span>Updates</span></h5>
-                <p className='font-interRegular md:text-xl text-base font-normal text-skin-muted'>Recieve first hand updates on latest financial news and new Fungazer products.</p>
-            </div>
-        </div>
-    </section>
-    {/* Resources Section End*/}
-    </>
-  )
+    const route = useRouter();
+
+    return (
+        <>
+            {/* Banner Section Start*/}
+            <section>
+                <div className='container mx-auto pt-40 pb-20 lg:pb-32 lg:pt-52 grid md:grid-cols-2 grid-cols-1 gap-8 items-center'>
+                    <div className="p-4">
+                        <h1 className='md:text-6xl text-4xl font-normal font-interRegular'><span className='text-skin-primary font-bold font-interBold'>Join</span> Our Community</h1>
+                        <p className='md:text-xl text-base font-medium mt-3 font-interRegular'>We are all about giving personal investors the tools and resources they need, to make informed decisions.</p>
+                        <Link href="#">
+                            <a className={`flex md:hidden w-full justify-end items-center mt-6 space-x-2 text-[20px] font-semibold lg:mt-0 font-interMedium ${route.pathname === '/our-community' ? 'hidden' : 'block'} `}>
+                                <span className="">Explore</span>
+                                <span className="text-skin-primary text-[2rem]">
+                                    <FaAngleRight strokeWidth={2} />
+                                </span>
+                            </a>
+                        </Link>
+                    </div>
+                    <div className="flex flex-wrap items-center md:justify-end justify-center gap-5 p-4">
+                        <button className='md:max-w-[200px] w-full justify-center font-interRegular bg-skin-dark text-skin-light hover:opacity-75 md:text-2xl text-lg font-semibold flex items-center space-x-3 py-3 px-7 rounded-md'><BsDiscord /> <span>Discord</span> </button>
+                        <button className='md:max-w-[200px] w-full justify-center font-interRegular bg-skin-buttonAccent hover:bg-skin-buttonMuted text-skin-light md:text-2xl text-lg font-semibold flex items-center space-x-3 py-3 px-7 rounded-md'><FaTelegramPlane /> <span>Telegram</span> </button>
+                    </div>
+                </div>
+            </section>
+            {/* Banner Section End*/}
+
+            {/* Resources Section Start*/}
+            <section className={`bg-[#E9E1FF] ${route.pathname === '/our-community' ? 'block' : 'hidden' }`}>
+                <div className='container mx-auto py-12 md:py-20 grid md:grid-cols-4 grid-cols-1 gap-2'>
+                    <div className='p-4'>
+                        <h5 className='font-interBold md:text-2xl text-xl font-bold flex space-x-2 items-center mb-3'><span className='text-skin-primary'><BiPolygon strokeWidth={2} /> </span> <span>Belong</span></h5>
+                        <p className='font-interRegular md:text-xl text-base font-normal text-skin-muted'>Get real-time advice and support from a community of experienced investors and don’t forget to give back!</p>
+                    </div>
+                    <div className='p-4'>
+                        <h5 className='font-interBold md:text-2xl text-xl font-bold flex space-x-2 items-center mb-3'><span className='text-skin-primary'><FiLayers strokeWidth={3} /> </span> <span>Resources</span></h5>
+                        <p className='font-interRegular md:text-xl text-base font-normal text-skin-muted'>Learn about finance, stocks, crypto and psychology behind building investment strategies and systems.</p>
+                    </div>
+                    <div className='p-4'>
+                        <h5 className='font-interBold md:text-2xl text-xl font-bold flex space-x-2 items-center mb-3'><span className='text-skin-primary'><BiPolygon strokeWidth={2} /> </span> <span>Black Box</span></h5>
+                        <p className='font-interRegular md:text-xl text-base font-normal text-skin-muted'>Stay ahead of the curve with market alerts powered by our proprietary algorithms.</p>
+                    </div>
+                    <div className='p-4'>
+                        <h5 className='font-interBold md:text-2xl text-xl font-bold flex space-x-2 items-center mb-3'><span className='text-skin-primary'><FiClock strokeWidth={3} /> </span> <span>Updates</span></h5>
+                        <p className='font-interRegular md:text-xl text-base font-normal text-skin-muted'>Recieve first hand updates on latest financial news and new Fungazer products.</p>
+                    </div>
+                </div>
+            </section>
+            {/* Resources Section End*/}
+        </>
+    )
 }
